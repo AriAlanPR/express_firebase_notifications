fa1c72756660388e522988e2da360a570d6299c5
AlanPerez <ari.perez@delbravo.com>
2024-06-24 10:51:59 -0500

    added example json calls

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,39 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/muleros/api/detalle', function(req, res, next) {
+    console.log(req.query);
+    detalles = {
+        "url": "http://gaetano.info",
+        "fecha_destino": 1719242472331,
+        "caja": "QF04TEC",
+        "origen": "similique quaerat totam",
+        "destino": "est est consequatur"
+    };
+
+    res.json(detalles);
+});
+
+router.get('/muleros/api/list', function(req, res, next) {
+    console.log(req.query);
+    detalles = [
+        {
+            "caja": "HA70JSM",
+            "status": "c",
+            "api_url_detalle": "https://geraldine.org",
+            "web_url": "http://destiny.name"
+        },
+        {
+            "caja": "LD30OLK",
+            "status": "a",
+            "api_url_detalle": "https://krystel.org",
+            "web_url": "http://krystina.biz"
+        }
+    ];
+
+    res.json(detalles);
+});
+
 router.post('/', function(req, res){
     console.log(req.body);
 
