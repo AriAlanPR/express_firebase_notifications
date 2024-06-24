d8cf898895393e9110a87788628a9f1e9dd7bcec
AlanPerez <ari.perez@delbravo.com>
2024-06-24 13:30:07 -0500

    built mock endpoints

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,21 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/api/auth/login', function(req, res, next) {
+    console.log(req.query);
+
+    res.json({
+        'token_type': 'Bearer',
+        'access_token': 'HA70JSM123456',
+        'expires_at': (new Date()).getMilliseconds(),
+        'token_id': 'HA70JSM11111',
+    });
+});
+
 router.get('/muleros/api/detalle', function(req, res, next) {
     console.log(req.query);
     detalles = {
-        "url": "http://gaetano.info",
+        "url": "http://btrack.org",
         "fecha_destino": 1719242472331,
         "caja": "QF04TEC",
         "origen": "similique quaerat totam",
@@ -25,18 +36,18 @@ router.get('/muleros/api/detalle', function(req, res, next) {
 
 router.get('/muleros/api/list', function(req, res, next) {
     console.log(req.query);
-    detalles = [
+    listado = [
         {
             "caja": "HA70JSM",
             "status": "c",
-            "api_url_detalle": "https://geraldine.org",
-            "web_url": "http://destiny.name"
+            "api_url_detalle": "https://btrack.org/muleros/api/detalle/<ID>",
+            "web_url": "https://btrack.org/mulero"
         },
         {
             "caja": "LD30OLK",
             "status": "a",
-            "api_url_detalle": "https://krystel.org",
-            "web_url": "http://krystina.biz"
+            "api_url_detalle": "https://btrack.org/muleros/api/detalle/<ID>",
+            "web_url": "https://btrack.org/mulero"
         }
     ];
 
@@ -47,6 +58,12 @@ router.post('/', function(req, res){
     console.log(req.body);
 
     res.json(req.body);
+});
+
+router.post('/api/auth/setDevice', function(req, res) {
+    console.log(req.body);
+
+    res.status(200).json(req.body);
 });
 
 module.exports = router;
