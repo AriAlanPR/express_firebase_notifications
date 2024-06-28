f772a23d2f746ccd855a81a8365a168f1242f1bd
AlanPerez <ari.perez@delbravo.com>
2024-06-28 18:40:11 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -10,39 +10,6 @@ router.get('/', function(req, res, next) {
     });
 });
 
-router.get('/muleros/api/detalle', function(req, res, next) {
-    console.log(req.query);
-    let detalles = {
-        "url": "http://btrack.org",
-        "fecha_destino": (1719242472331).toString(),
-        "caja": "QF04TEC",
-        "origen": "similique quaerat totam",
-        "destino": "est est consequatur"
-    };
-
-    res.json(detalles);
-});
-
-router.get('/muleros/api/list', function(req, res, next) {
-    console.log(req.query);
-    let listado = [
-        {
-            "caja": "HA70JSM",
-            "status": "c",
-            "api_url_detalle": "https://btrack.org/muleros/api/detalle/<ID>",
-            "web_url": "https://btrack.org/mulero"
-        },
-        {
-            "caja": "LD30OLK",
-            "status": "a",
-            "api_url_detalle": "https://btrack.org/muleros/api/detalle/<ID>",
-            "web_url": "https://btrack.org/mulero"
-        }
-    ];
-
-    res.json(listado);
-});
-
 router.post('/', function(req, res){
     console.log(req.body);
 
@@ -53,7 +20,7 @@ router.post('/api/auth/login', function(req, res) {
     console.log(req.body);
     let loginResponse = {
         token_type: 'Bearer',
-        access_token: 'HA70JSM123456',
+        access_token: 'AT-HA70JSM123456',
         expires_at: (new Date()).getTime().toString(),
         token_id: 'HA70JSM11111',
     };
@@ -70,6 +37,46 @@ router.post('/api/auth/setDevice', function(req, res) {
     response = JSON.stringify(response);
 
     res.status(200).json(response);
+});
+
+router.post('/api/auth/muleros/getDetalleApp/:id', function(req, res, next) {
+    console.log(`req.params: ${req.params}`);
+    console.log(`req body: ${req.body}`);
+
+    let detalles = {
+        "id": req.params.id,
+        "url": "http://btrack.org",
+        "fecha": (1719242472331).toString(),
+        "caja": "QF04TEC",
+        "transportista": "lorem ipsum",
+        "origen": "similique quaerat totam",
+        "destino": "est est consequatur"
+    };
+
+    res.json(detalles);
+});
+
+router.post('/api/auth/muleros/getListApp', function(req, res, next) {
+    console.log(req.body);
+
+    let listado = [
+        {
+            "caja": "HA70JSM",
+            "status": "c",
+            "api_url_detalle": "http://192.168.10.105:3000/api/auth/muleros/getDetalleApp/1",
+            "web_url": "https://btrack.org/mulero"
+        },
+        {
+            "caja": "LD30OLK",
+            "status": "a",
+            "api_url_detalle": "http://192.168.10.105:3000/api/auth/muleros/getDetalleApp/2",
+            "web_url": "https://btrack.org/mulero"
+        },
+    ];
+
+    res.json({
+        list: listado
+    });
 });
 
 module.exports = router;
